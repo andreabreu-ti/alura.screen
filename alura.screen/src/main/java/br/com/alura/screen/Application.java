0916e996b4dd6d570e31ef57b33b47120e1d1f07
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2024-02-20 15:17:53 -0300

    Modelando Dados Serie

--- alura.screen/src/main/java/br/com/alura/screen/Application.java
+++ alura.screen/src/main/java/br/com/alura/screen/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screen.model.DadosSerie;
 import br.com.alura.screen.service.ConsumoAPI;
+import br.com.alura.screen.service.ConverteDados;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner{
@@ -18,7 +20,12 @@ public class Application implements CommandLineRunner{
 
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=ed670f10"); 
-		System.out.println(json);
+		//System.out.println(json);
+		
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+		
 		
 		//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		//		System.out.println(json);
